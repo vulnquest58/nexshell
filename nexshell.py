2483ffc2e6eb0625d57eaccaad64512d0f37a738
vulnquest58 <vulnquest@gmail.com>
2026-07-06 22:54:18 +0100

    style: change ASCII banner to the new skull art

--- nexshell.py
+++ nexshell.py
@@ -16,15 +16,26 @@
 __program__   = "nexshell"
 __version__   = "1.0.0"
 __author__    = "vulnquest58"
-__banner__    = r"""
-   ███╗   ██╗███████╗██╗  ██╗███████╗██╗  ██╗███████╗██╗     ██╗
-   ████╗  ██║██╔════╝╚██╗██╔╝██╔════╝██║  ██║██╔════╝██║     ██║
-   ██╔██╗ ██║█████╗   ╚███╔╝ ███████╗███████║█████╗  ██║     ██║
-   ██║╚██╗██║██╔══╝   ██╔██╗ ╚════██║██╔══██║██╔══╝  ██║     ██║
-   ██║ ╚████║███████╗██╔╝ ██╗███████║██║  ██║███████╗███████╗███████╗
-   ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
+__banner__    = r'''
+             .o oOOOOOOOo                                            OOOo
+             Ob.OOOOOOOo  OOOo.      oOOo.                      .adOOOOOOO
+             OboO"""""""""""".OOo. .oOOOOOo.    OOOo.oOOOOOo.."""""""""'OO
+             OOP.oOOOOOOOOOOO "POOOOOOOOOOOo.   `"OOOOOOOOOP,OOOOOOOOOOOB'
+             `O'OOOO'     `OOOOo"OOOOOOOOOOO` .adOOOOOOOOO"oOOO'    `OOOOo
+             .OOOO'            `OOOOOOOOOOOOOOOOOOOOOOOOOO'            `OO
+             OOOOO                 '"OOOOOOOOOOOOOOOO"`                oOO
+            oOOOOOba.                .adOOOOOOOOOOba               .adOOOOo.
+           oOOOOOOOOOOOOOba.    .adOOOOOOOOOO@^OOOOOOOba.     .adOOOOOOOOOOOO
+          OOOOOOOOOOOOOOOOO.OOOOOOOOOOOOOO"`  '"OOOOOOOOOOOOO.OOOOOOOOOOOOOO
+          "OOOO"       "YOoOOOOMOIONODOO"`  .   '"OOROAOPOEOOOoOY"     "OOO"
+             Y           'OOOOOOOOOOOOOO: .oOOo. :OOOOOOOOOOO?'         :`
+             :            .oO%OOOOOOOOOOo.OOOOOO.oOOOOOOOOOOOO?          
+                          oOOP"%OOOOOOOOoOOOOOOO?oOOOOO?OOOO"OOo
+                          '%o  OOOO"%OOOO%"%OOOOO"OOOOOO"OOO':
+                               `$"  `OOOO' `O"Y ' `OOOO'  o              
+                                      OP"          : o                                 
               Nexus of Shell Operations  ·  Elite Reverse Shell Commander
-"""
+'''
 
 import os, io, re, sys, ssl, time, gzip, json, shlex, queue, struct
 import shutil, socket, signal, base64, secrets, tarfile, logging
